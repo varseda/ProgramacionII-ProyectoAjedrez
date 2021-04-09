1185bc8cdaed41cd85e0018d9fcf0ac2ff8b8057
Jose Varela Valladares <61077942+varseda@users.noreply.github.com>
2021-04-08 19:25:26 -0600

    Creacion etiquetas de posiciones
    Se crearon las etiquetas de pocisiones en el tablero

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from string import ascii_letters
 
 # Creacion de centana principal
 root = Tk()
@@ -9,10 +10,32 @@ def changePosition():
     buttonP.grid(row=5, column=3)
 
 
+# Creacion del frame tablero con posiciones
+# ------------------------------------------------------------------------------
+frameBoardPositions = Frame(root, background="black")
+frameBoardPositions.grid(row=0, column=0)
+# ------------------------------------------------------------------------------
+
+# Creacion etiquetas de posiciones
+# ------------------------------------------------------------------------------
+listPositionsLetters = []
+listPositionsNumbers = []
+
+for i in range(0, 8):
+
+    listPositionsLetters.append(
+        Label(frameBoardPositions, foreground="white", background="black", text=ascii_letters[i], font=("Verdana", 12)))
+    listPositionsLetters[i].grid(row=0, column=(i + 1))
+
+    listPositionsNumbers.append(
+        Label(frameBoardPositions, foreground="white", background="black", text=str(i), font=("Verdana", 12)))
+    listPositionsNumbers[i].grid(row=(i + 1), column=0, padx=5, pady=5)
+# ------------------------------------------------------------------------------
+
 # Creacion del frame tablero
 # ------------------------------------------------------------------------------
-frameBoard = Frame(root)
-frameBoard.grid(row=0, column=0, padx=5, pady=5)
+frameBoard = Frame(frameBoardPositions)
+frameBoard.grid(row=1, column=1, rowspan=8, columnspan=8)
 
 listBoard = []
 image0 = PhotoImage(file="Images/frame0.png")
@@ -31,30 +54,33 @@ for i in range(0, 8):
                     Label(frameBoard, image=image0, borderwidth=0))
                 listBoard[i][j].grid(row=i, column=j)
             else:
-                listBoard[i].append(Label(frameBoard, image=image1, borderwidth=0))
+                listBoard[i].append(
+                    Label(frameBoard, image=image1, borderwidth=0))
                 listBoard[i][j].grid(row=i, column=j)
 
         else:
 
             if j % 2 == 0:
-                listBoard[i].append(Label(frameBoard, image=image1, borderwidth=0))
+                listBoard[i].append(
+                    Label(frameBoard, image=image1, borderwidth=0))
                 listBoard[i][j].grid(row=i, column=j)
             else:
-                listBoard[i].append(Label(frameBoard, image=image0, borderwidth=0))
+                listBoard[i].append(
+                    Label(frameBoard, image=image0, borderwidth=0))
                 listBoard[i][j].grid(row=i, column=j)
 # ------------------------------------------------------------------------------
 
 
 # Creacion del frame de estado
 # ------------------------------------------------------------------------------
-frameState = Frame(root)
-frameState.grid(row=0, column=1, padx=5, pady=5)
+# frameState = Frame(root)
+# frameState.grid(row=0, column=1, padx=5, pady=5)
 
-labelPlayer0 = Label(frameState, text="Jugador #1", font=16)
-labelPlayer0.grid(row=0, column=0)
+# labelPlayer0 = Label(frameState, text="Jugador #1", font=16)
+# labelPlayer0.grid(row=0, column=0)
 
-labelPlayer1 = Label(frameState, text="Jugador #2", font=16)
-labelPlayer1.grid(row=1, column=0)
+# labelPlayer1 = Label(frameState, text="Jugador #2", font=16)
+# labelPlayer1.grid(row=1, column=0)
 # ------------------------------------------------------------------------------
 
 buttonP = Button(frameBoard, text="HOLA", command=changePosition)
